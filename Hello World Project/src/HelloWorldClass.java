bb3ccf1052ae35cf29c96cbe3af3b18f0c855a6c
APCS2nd <APCS2nd@MHS-LT9-23>
2019-08-21 12:17:16 -0700

    This For APCS 4th. Complete Hello World Project

---
+++ Hello World Project/src/HelloWorldClass.java
@@ -0,0 +1,13 @@
+/*Steven Yao
+ * 8/19/19
+ * Print Hello World and Export it
+ */
+public class HelloWorldClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
